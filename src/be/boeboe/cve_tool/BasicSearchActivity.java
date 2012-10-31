b93e91cd96c7835ffad075517bb674003f579e7b
Bart Van Bos <bart.vanbos@technicolor.com>
2012-10-31 15:04:15 +0100

    Add CVE_Tools Eclipse-enabled Android project

---
+++ src/be/boeboe/cve_tool/BasicSearchActivity.java
@@ -0,0 +1,89 @@
+package be.boeboe.cve_tool;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class BasicSearchActivity extends Activity {
+  private ImageButton fSearchButton;
+  private TextView fSearchTerm;
+  
+  private OnClickListener searchButtonListener = new OnClickListener() {
+    @Override
+    public void onClick(View v) {
+      System.out.println("Going to search for: " + fSearchTerm.getText());
+    }
+  };
+  
+  private final TextWatcher textWatcher = new TextWatcher() {
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+      updateUI();
+    }
+
+  };
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_basic_search);
+    getActionBar().setDisplayHomeAsUpEnabled(true);
+    
+    fSearchButton = (ImageButton) findViewById(R.id.do_basic_search);
+    fSearchTerm = (TextView) findViewById(R.id.search_term);
+    
+    fSearchButton.setOnClickListener(searchButtonListener);
+    fSearchTerm.addTextChangedListener(textWatcher);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    updateUI();
+  }
+
+  private void updateUI() {
+    String searchTermText = fSearchTerm.getText().toString();
+    if (TextUtils.isEmpty(searchTermText)) {
+      fSearchButton.setEnabled(false);
+    } else {
+      fSearchButton.setEnabled(true);
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_basic_search, menu);
+    return true;
+  }
+  
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        NavUtils.navigateUpFromSameTask(this);
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+}
---
+++ src/be/boeboe/cve_tool/MainActivity.java
@@ -0,0 +1,39 @@
+package be.boeboe.cve_tool;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+  
+  private Button fBasicSearchButton;
+  private Button fAdvancedSearchButton;
+
+  private OnClickListener basicSearchButtonListener = new OnClickListener() {
+    @Override
+    public void onClick(View v) {
+      startActivity(new Intent(getApplicationContext(), BasicSearchActivity.class));
+    }
+  };
+  
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    
+    fBasicSearchButton = (Button) findViewById(R.id.jump_cvs_basic_search);
+    fAdvancedSearchButton = (Button) findViewById(R.id.jump_cvs_advanced_search);
+    
+    fBasicSearchButton.setOnClickListener(basicSearchButtonListener);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+  }
+}
